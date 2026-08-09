43a6c6b882ca5dd88487734250aa690bd7d7ff65
Shyamlal Ram <krshyamlal14@gmail.com>
2026-08-09 18:39:35 +0000

    add leap year code

--- 01-java-basics/solutions/LeapYear.java
+++ 01-java-basics/solutions/LeapYear.java
@@ -1,1 +1,22 @@
 //Input a year and find whether it is a leap year or not.
+import java.util.*;
+public class LeapYear{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter year: ");
+	    int year = sc.nextInt();
+
+		boolean ans = isLeap(year);
+		System.out.println(ans);
+
+        sc.close();
+    }
+    static boolean isLeap(int year){
+	    //1st approach.
+	    boolean leap = year % 100 == 0 ? (year % 400 == 0 ? true : false) : (year % 4 == 0 ? true : false);
+	    return leap;
+	    //2nd approach.
+	    //return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+	}
+}
