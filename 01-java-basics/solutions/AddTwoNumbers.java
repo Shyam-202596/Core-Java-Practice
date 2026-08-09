c6690203ea524598055847f22b8b580a63f81261
Shyamlal Ram <krshyamlal14@gmail.com>
2026-08-09 18:44:57 +0000

    add sum method

---
+++ 01-java-basics/solutions/AddTwoNumbers.java
@@ -0,0 +1,16 @@
+//Take two numbers and print the sum of both.
+import java.util.*;
+public class AddTwoNumbers {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int num2 = sc.nextInt();
+        int ans = add(num1, num2);    
+        System.out.println("Sum of both the number is: " + ans);
+    }
+    static int add(int num1, int num2){
+	    return num1 + num2;
+	}
+}
