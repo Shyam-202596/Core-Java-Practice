a968936eb4782af5619189046b1f6e969231419e
Shyamlal Ram <krshyamlal14@gmail.com>
2026-09-24 08:39:23 +0000

    formatted IP Address code

--- 31-networking/solutions/IPAddress.java
+++ 31-networking/solutions/IPAddress.java
@@ -1,11 +1,15 @@
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 public class IPAddress {
     public static void main(String[] args) throws java.lang.Exception {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a website name: ");
         String site = sc.nextLine();
         try{
-            InetAddress ip1 = InetAddress.getByName(site);
-            System.out.println("IP Addressid: " + ip);
+            InetAddress ip = InetAddress.getByName(site);
+            System.out.println("IP Address: " + ip);
         }catch(UnknownHostException ue){
             System.out.println("Website not found");
         }
